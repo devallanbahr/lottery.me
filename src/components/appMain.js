d56a0544227d0fc96575202590993bf79b2018b7
Allan <allanbahrdev@gmail.com>
2021-11-18 14:55:34 -0300

    master

--- src/components/appMain.js
+++ src/components/appMain.js
@@ -21,7 +21,7 @@ class App extends Component {
             let numbersWon = console.log(
               `Você acertou: ${sortedNumbersArr[i]}!`
             );
-            console.log(sortedNumbersArr[i].length);
+            console.log(sorted[i]);
           }
         }
         selectedNumbersArr = [];
